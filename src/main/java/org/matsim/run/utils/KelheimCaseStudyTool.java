90964713fc4222ea81308dcd6b95c9be28b7e6cb
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2021-12-09 14:31:57 +0100

    Enable multiple speed limits for DRT vehicles

--- src/main/java/org/matsim/run/utils/KelheimCaseStudyTool.java
+++ src/main/java/org/matsim/run/utils/KelheimCaseStudyTool.java
@@ -9,12 +9,10 @@ public class KelheimCaseStudyTool {
     public static void setConfigFile(Config config, DrtConfigGroup drtConfig, AV_SERVICE_AREAS avServiceAreas) {
         // Set drt related things (vehicle file, stops file)
         if (avServiceAreas == AV_SERVICE_AREAS.HOHENPFAHL) {
-            drtConfig.setVehiclesFile("av-vehicles-Hohenpfahl-DP-AS.xml");
             drtConfig.setTransitStopFile("av-stops-Hohenpfahl-DP-AS.xml");
         }
 
         if (avServiceAreas == AV_SERVICE_AREAS.BAUERNSIEDLUNG) {
-            drtConfig.setVehiclesFile("av-vehicles-Bauernsiedlung-DP-AS.xml");
             drtConfig.setTransitStopFile("av-stops-Bauernsiedlung-DP-AS.xml");
         }
 
