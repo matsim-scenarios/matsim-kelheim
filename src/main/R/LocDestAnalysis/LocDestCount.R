0e8190ea28f96787575c9953e15246adb0adbd78
Tomke Louisa Stöhr <t.stoehr@campus.tu-berlin.de>
2022-02-24 12:09:33 +0100

    First Version of LocDestCount.R, result csv Datei with  strings and Fahrtencounts

---
+++ src/main/R/LocDestAnalysis/LocDestCount.R
@@ -0,0 +1,52 @@
+setwd("/Users/tomkelouisa/Documents/VSP/Kehlheim/src/main/R/LocDestAnalysis")
+#Daten Stopdaten einlesen
+stops <- read.csv("kelheim-drt-stops-locations.csv", stringsAsFactors = FALSE, header = TRUE, encoding = "UTF-8")
+movements <- read.csv("KEXI-base-case.passingQ.250.drt_legs_drt.csv", stringsAsFactors = FALSE, header = TRUE, encoding = "UTF-8", sep= ";")
+
+
+
+#Sortiert erst Dataframe erst nach "fromLinkId" und dann nach "tiLinkId"
+sortedMovement <- movements[order(movements$fromLinkId,movements$toLinkId), ]
+
+#erstellt Vektoren / vll noch eone schönere Lösung möglich
+laenge <- length(movements$fromLinkId)
+fromLink <-character(0)
+toLink <- character(0)
+anzahlFahrten <- character(0)
+
+
+
+Fahrten <- 1 # gibt Anzahl der Fahrten zwischen zwei Links an
+connection <- c(1,1) # Vektor mit einem from und einem to Link drin
+
+#Iteration durch sorted Movement, wobei paarweise die Tuple (hier Vektoren, connection und newConnection) verglichen werden
+# sind sie identisch, wird Fahrten+1 gerechnet, ansonsten werden di eTuple abgespeichert und das nächste Tupel wird verglichen
+for(row in 1:laenge){
+  newConnection <- c(sortedMovement[row,"fromLinkId"],sortedMovement[row,"toLinkId"]) # nächster Vektor mit einem from und einem to Link drin
+  if (identical(connection,newConnection)){
+    Fahrten <- Fahrten + 1
+    print("Jujeee")
+    }
+  else{
+      fromLink <- c(fromLink,connection[1])
+      toLink <- c(toLink,connection[2])
+      anzahlFahrten <- c(anzahlFahrten,Fahrten)
+      Fahrten <- 1
+      connection <- newConnection
+
+    }
+}
+#mögliche letztes Tupel abfangen
+if (Fahrten>=2){
+  fromLink <- c(fromLink,connection[1])
+  toLink <- c(toLink,connection[2])
+  anzahlFahrten <- c(anzahlFahrten,Fahrten)
+}
+# in Datadfame speichern und als csv Datei abspeichern
+class.df <- data.frame(fromLink,toLink,anzahlFahrten,stringsAsFactors = FALSE)
+print(class.df)
+setwd("/Users/tomkelouisa/Documents/VSP/Kehlheim/src/main/R/LocDestAnalysis")
+write.csv(class.df, "LinktoLinkAnzahl.csv")
+
+
+
