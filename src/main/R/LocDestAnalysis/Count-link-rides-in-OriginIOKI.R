ddd331cb50118460ae3f98ddaf02cdc6f33f0990
Tomkelou <t.stoehr@campus.tu-berlin.de>
2022-03-17 20:29:58 +0100

    Zeitbereich kann bei beiden Realdaten eingestellt werden

--- src/main/R/LocDestAnalysis/Count-link-rides-in-OriginIOKI.R
+++ src/main/R/LocDestAnalysis/Count-link-rides-in-OriginIOKI.R
@@ -10,6 +10,9 @@ filePath <- "/Users/tomkelouisa/Documents/VSP/Kehlheimfiles" # hier sollen die b
 originfilename <- "IOKI_Rides_202006_202105.csv" # Filename der Realdaten
 haltestellenFile <- "KEXI_Haltestellen_Liste_Kelheim.csv" # Filename der Haltestellenliste
 nameAnalyseFile <- "IOKI-Origin-drt-count-Analysis-KEXI.tsv" # Filename des Analyseoutputfiles, in tsv
+startdate <- "19-12-2020"  # wenn das ganze File ausgewertet werden soll, schreibe hier -1 rein
+tage <- 1 # Anzahl an Tagen die ausgewertet werden sollen
+
 
 setwd(programPath)
 
@@ -18,6 +21,16 @@ source("countdrivenstops.R")
 setwd(filePath)
 # Daten von IOKI einlesen
 IOKIDataframe <- read.csv(originfilename, stringsAsFactors = FALSE, header = TRUE, encoding = "UTF-8", sep= ";")
+
+
+IOKIDataframe <- IOKIDataframe %>% mutate(Ankunftszeit = dmy_hms(Ankunftszeit))
+# den Zeitbereich auswählen
+if (startdate!=-1){
+  d <- interval(dmy(startdate),dmy(startdate)+days(x=tage))
+  IOKIDataframe <- IOKIDataframe %>% filter(Ankunftszeit %within% d)
+}
+
+
 
 stops <- read.csv(haltestellenFile, stringsAsFactors = FALSE, header = TRUE, encoding = "UTF-8", sep= ";")
 
--- src/main/R/LocDestAnalysis/Count-link-rides-in-OriginVIA.R
+++ src/main/R/LocDestAnalysis/Count-link-rides-in-OriginVIA.R
@@ -2,6 +2,7 @@ library(geosphere)
 library(tidyverse)
 library(dplyr)
 library(terra)
+library(lubridate)
 ######################
 # dies sind die Variablen, die man für den eigenen Gebrauch anpassen muss
 programPath <- "/Users/tomkelouisa/Documents/VSP/Kehlheim/src/main/R/LocDestAnalysis" # hier sollen die Funktionen neareststop.R und countdrivenstops.R liegen
@@ -9,6 +10,8 @@ filePath <- "/Users/tomkelouisa/Documents/VSP/Kehlheimfiles" # hier sollen die b
 originfilename <- "VIA_Rides_202106_202201origin.csv" # Filename der Realdaten
 haltestellenFile <- "KEXI_Haltestellen_Liste_Kelheim.csv" # Filename der Haltestellenliste
 nameAnalyseFile <- "Via-Origin-drt-count-Analysis-KEXI.tsv" # Filename des Analyseoutputfiles, in tsv
+startdate <- "2021-07-01"  # wenn das ganze File ausgewertet werden soll, schreibe hier -1 rein
+tage <- 1 # Anzahl an Tagen die ausgewertet werden sollen
 
 setwd(programPath)
 source("neareststop.R")
@@ -19,6 +22,14 @@ source("countdrivenstops.R")
 setwd(filePath)
 # Daten von VIA einlesen
 viaDataframe <- read.csv(originfilename, stringsAsFactors = FALSE, header = TRUE, encoding = "UTF-8", sep= ";")
+
+viaDataframe <- viaDataframe %>% mutate(Actual.DO.time = ymd_hms(Actual.DO.time))
+
+if (startdate!=-1){
+  d <- interval(ymd(startdate),ymd(startdate)+days(x=tage))
+  viaDataframe <- viaDataframe %>% filter(Actual.DO.time %within% d)
+}
+
 
 
 if(("fromstopID" %in% colnames(viaDataframe))==FALSE){
