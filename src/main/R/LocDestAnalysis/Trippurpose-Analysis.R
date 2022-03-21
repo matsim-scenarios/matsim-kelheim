b2d680f9e51348bcc3b4e15df9ab2800cce6c010
Tomkelou <t.stoehr@campus.tu-berlin.de>
2022-03-21 13:34:55 +0100

    add new bar charts for all of the traveling modes and specific ones just for drt

--- src/main/R/LocDestAnalysis/Trippurpose-Analysis.R
+++ src/main/R/LocDestAnalysis/Trippurpose-Analysis.R
@@ -1,10 +1,12 @@
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
+library(plotly)
 
 require(readr)
 filePath<-"/Users/tomkelouisa/Documents/VSP/Kehlheimfiles"
 filename <- "kelheim.output_trips.csv.gz"
+
 
 setwd(filePath)
 TripDataframe <- read.csv(filename, stringsAsFactors = FALSE, header = TRUE, encoding = "UTF-8", sep= ";")
@@ -18,37 +20,136 @@ counts <- TripDataframe %>%
     group_by(main_mode,end_activity_type) %>%
         summarise(Anzahl = n())
 
+interactiveMode<-TRUE
+#######################
+#Analyse nur für drt
+drtanalyse <- TripDataframe %>%
+  filter(main_mode=="drt")
+print(length(drtanalyse$main_mode))
+
+plot <-ggplot(drtanalyse,aes(x=end_activity_type))+
+  geom_bar(fill= "steelblue")+
+  xlab("Trippurpose")+
+  ggtitle("Trippurposes of drt usage")+
+  ylab("Number if trippurposes ")+
+  theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5))
+
+
+plotFile <-paste(filePath,"/drt-Wegezwecke.png",sep="")
+paste("printing plot to ", plotFile)
+png(plotFile, width = 1200, height = 800)
+plot
+dev.off()
+if(interactiveMode){
+  ggplotly(plot)
+}
+
+plot1 <-ggplot(drtanalyse,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
+  geom_bar()+
+  xlab("Trippurpose")+
+  ylab("Traveling mode")+
+  ggtitle("Trippurposes of drt usage")+
+  scale_fill_hue(l=40)+
+  #theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5))
+
+
+plotFile <-paste(filePath,"/drt_as_Stack_Wegzwecke.png",sep="")
+paste("printing plot to ", plotFile)
+png(plotFile, width = 1200, height = 800)
+plot1
+dev.off()
+if(interactiveMode){
+  ggplotly(plot1)
+}
+
+dudu<- FALSE
+if (dudu){
+
+
 #auch gut, aber zu groß,
-ggplot(counts, aes(x=main_mode, y=Anzahl, fill=end_activity_type))+
+p1<-ggplot(counts, aes(x=main_mode, y=Anzahl, fill=end_activity_type))+
     geom_bar( stat="identity",position ="stack")+
     xlab("Mainmodes")+
     ylab("Anzahl der Wegzwecke")+
+    ggtitle("Trippurposes of drt usage")+
     facet_grid(~end_activity_type)
 
 
-ggsave("example.pdf",width=10,height=10)
+plotFile <-paste(filePath,"/try.pdf",sep="")
+paste("printing plot to ", plotFile)
+pdf(plotFile, width = 1200, height = 800)
+p1
+dev.off()
+if(interactiveMode){
+  ggplotly(p1)
+}
+
+#ggsave("example.pdf",width=10,height=10)
 
 
-# gar nicht so schlecht
-ggplot(TripDataframe, aes(x=main_mode, y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
+##############
+#Stackbalkendiagramm, die Wegzwecke auf Fortbewegungart aufgetragen
+#drt sehr schlecht erkennbar, da so geringe Anzahl
+p2 <-ggplot(TripDataframe, aes(x=main_mode, y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
   geom_bar( )+
   xlab("Fortbewegungsart")+
   ylab("Anzahl der Wegzwecke")+
+  ggtitle("Trippurposes of traveling modes")+
   scale_y_continuous(breaks= seq(0,100000,5000))+
   labs(fill="Wegezwecke")+
   scale_fill_hue(l=40)
-  #geom_text(aes(label=stat(count)),stat="count",position=position_stack(vjust=0.5))
 
-ggsave("try.png",width=10,height=10)
+plotFile <-paste(filePath,"/Wegezwecke_Fortbewegungsmittel.png",sep="")
+paste("printing plot to ", plotFile)
+png(plotFile, width = 1200, height = 800)
+p2
+dev.off()
+if(interactiveMode){
+  ggplotly(p2)
+}
 
 
-# das ist gut
-ggplot(counts, aes(x=main_mode, y=Anzahl, fill=end_activity_type))+
+
+################
+# Veranschauling der Fortbewegungsarten im Bezug auf einen Wegzweck
+p3<-ggplot(counts, aes(x=main_mode, y=Anzahl, fill=end_activity_type))+
   geom_bar( stat="identity",position ="stack")+
   xlab("Mainmodes")+
   ylab("Anzahl der Wegzwecke")+
   facet_wrap(~end_activity_type, scale="free")
 
-################ drt
+plotFile <-paste(filePath,"/Fortbewegungsmittel_pro_Wegzweck.png",sep="")
+paste("printing plot to ", plotFile)
+png(plotFile, width = 1200, height = 800)
+p3
+dev.off()
+if(interactiveMode){
+  ggplotly(p3)
+}
+################
+#Veranschaulichung der Wegezwecke im Bezug auf eine Fortbewegungsart
 
+p4 <-ggplot(counts, aes(x=end_activity_type,y=Anzahl, fill=main_mode))+
+  geom_bar( stat="identity",position ="stack")+
+  ylab("Mainmodes")+
+  xlab("Anzahl der Wegzwecke")+
+  facet_wrap(~main_mode, scale="free")+
+  theme(axis.text.x = element_text(angle = 45,hjust=1))
+
+
+
+plotFile <-paste(filePath,"/Weckzweck_pro_Fortbewegungsmittel.png",sep="")
+paste("printing plot to ", plotFile)
+png(plotFile, width = 1200, height = 800)
+p4
+dev.off()
+if(interactiveMode){
+  ggplotly(p4)
+}
+
+}
+
+## simplify the plot edu=combine
 
