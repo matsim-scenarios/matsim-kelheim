5a61416043a60572cf76505e5a4db84fe6a28da2
tschlenther <schlenther@vsp.tu-berlin.de>
2022-04-07 14:00:30 +0200

    adjust paths and file names in trip purpose analysis

--- src/main/R/LocDestAnalysis/Trippurpose-Analysis.R
+++ src/main/R/LocDestAnalysis/Trippurpose-Analysis.R
@@ -7,15 +7,31 @@ library(ggpubr)
 library(patchwork)
 library(RColorBrewer)
 
-
 require(readr)
-filePath<-"/Users/tomkelouisa/Documents/VSP/Kehlheimfiles/Wegezweckeplots" # the plots are going to be saved here
-filePaths<-"/Users/tomkelouisa/Documents/VSP/Kehlheimfiles" # the output_trips file should be there
-filename <- "kelheim.output_trips.csv.gz"
+
+##################################################################################################################################
+##INPUTS
+
+#  !!!INCLUDE TRAILING SLASH!!!
+#the output_trips file should be there
+
+#runDirectory <- "D:/svn/public-svn/matsim/scenarios/countries/de/kelheim/projects/KelRide/AVServiceAreas/output/KEXI-base-case/"
+runDirectory <- "Z:/net/ils/matsim-kelheim/kelheim-case-study/KEXI-with-av/run-asv-0.7_marginal-5e-5-BAUERNSIEDLUNG/" 
+
+interactiveMode <-FALSE
+
+#setwd(runDirectory) # !not necessary but try if something fails!
+
+##################################################################################################################################
+##output directory is automatically set and created
+outputDir <- paste(runDirectory, "analysis-trip-purposes", sep = "") # the plots are going to be saved here
+if(!file.exists(outputDir)){
+  print("creating analysis sub-directory")
+  dir.create(outputDir)  
+}
 
 
-setwd(filePaths)
-TripDataframe <- read.csv(filename, stringsAsFactors = FALSE, header = TRUE, encoding = "UTF-8", sep= ";")
+TripDataframe <- read.csv(list.files(runDirectory, pattern = "*.output_trips.csv.gz", full.names = T, include.dirs = F), stringsAsFactors = FALSE, header = TRUE, encoding = "UTF-8", sep= ";")
 #integer am Ende der en
 
 TripDataframe <- TripDataframe %>%
@@ -26,9 +42,14 @@ counts <- TripDataframe %>%
     group_by(main_mode,end_activity_type) %>%
         summarise(count = n())
 
-interactiveMode<-FALSE
+
+
+
+##################################################################################################################################################
 #######################
-#Analyse nur für drt
+
+
+#analyze drt mode
 drtanalyse <- TripDataframe %>%
   filter(main_mode=="drt")
 print(length(drtanalyse$main_mode))
@@ -37,8 +58,8 @@ print(length(drtanalyse$main_mode))
 #drt Balkendiagramm
 plot <-ggplot(drtanalyse,aes(x=end_activity_type))+
   geom_bar(fill= "steelblue")+
-  xlab("Trippurpose")+
-  ggtitle("Trip purposes of drt usage")+
+  xlab("Trip purpose")+
+  ggtitle("Trip purposes of rides with conventional KEXI")+
   ylab("Number of trips ")+
   theme(axis.text.x = element_text(angle = 45,hjust=1))+
   theme(plot.title = element_text(hjust = 0.5))+
@@ -47,7 +68,7 @@ plot <-ggplot(drtanalyse,aes(x=end_activity_type))+
   geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5),text=element_text(size=20),color="white")
 
 
-plotFile <-paste(filePath,"/drt-Wegezwecke.png",sep="")
+plotFile <-paste(outputDir,"/drt_trip_purposes.png",sep="")
 paste("printing plot to ", plotFile)
 png(plotFile, width = 1200, height = 800)
 plot
@@ -55,13 +76,14 @@ dev.off()
 if(interactiveMode){
   ggplotly(plot)
 }
+
 ##############
 #drt Stackbalkendiagramm
 plot1 <-ggplot(drtanalyse,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
   geom_bar()+
-  xlab("Trippurpose")+
+  xlab("Trip purpose")+
   ylab("Traveling mode")+
-  ggtitle("Trippurposes of drt usage")+
+  ggtitle("Trip purposes of trips with conventional KEXI")+
   scale_fill_manual(values = c("#A6CEE3", "#6EAACF", "#3687BC", "#4190AA" ,"#7EBA98" ,"#AADB84" ,"#76C15D", "#41A737", "#6D9E4C", "#C09B78", "#F88A8A", "#EE5656", "#E42123", "#EC5439", "#F6985B", "#FDB35B" ,"#FE992D", "#FF7F00")) +
   #theme_ipsum()+
   theme(text=element_text(size=20))+
@@ -70,7 +92,7 @@ plot1 <-ggplot(drtanalyse,aes(x=main_mode,y=stat(count), group=factor(end_activi
   geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5))
 
 
-plotFile <-paste(filePath,"/drt_as_Stack_Wegzwecke.png",sep="")
+plotFile <-paste(outputDir,"/drt_trip_purposes_stacked.png",sep="")
 paste("printing plot to ", plotFile)
 #ggsave(plotFile)
 png(plotFile, width = 1200, height = 800)
@@ -80,25 +102,78 @@ if(interactiveMode){
   ggplotly(plot1)
 }
 
-#ggsave("example.pdf",width=10,height=10)
+#analyze av mode
+avanalyse <- TripDataframe %>%
+  filter(main_mode=="av")
+print(length(drtanalyse$main_mode))
+
+####################
+#drt Balkendiagramm
+plot <-ggplot(avanalyse,aes(x=end_activity_type))+
+  geom_bar(fill= "steelblue")+
+  xlab("Trip purpose")+
+  ggtitle("Trip purposes of rides with KEXI AV")+
+  ylab("Number of trips ")+
+  theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  #theme_ipsum()+
+  theme(text=element_text(size=20))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5),text=element_text(size=20),color="white")
+
+
+plotFile <-paste(outputDir,"/av_trip_purposes.png",sep="")
+paste("printing plot to ", plotFile)
+png(plotFile, width = 1200, height = 800)
+plot
+dev.off()
+if(interactiveMode){
+  ggplotly(plot)
+}
+
+##############
+#drt Stackbalkendiagramm
+plot1 <-ggplot(avanalyse,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
+  geom_bar()+
+  xlab("Trip purpose")+
+  ylab("Traveling mode")+
+  ggtitle("Trip purposes of trips with KEXI AV")+
+  scale_fill_manual(values = c("#A6CEE3", "#6EAACF", "#3687BC", "#4190AA" ,"#7EBA98" ,"#AADB84" ,"#76C15D", "#41A737", "#6D9E4C", "#C09B78", "#F88A8A", "#EE5656", "#E42123", "#EC5439", "#F6985B", "#FDB35B" ,"#FE992D", "#FF7F00")) +
+  #theme_ipsum()+
+  theme(text=element_text(size=20))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  #theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5))
+
+
+plotFile <-paste(outputDir,"/av_trip_purposes_stacked.png",sep="")
+paste("printing plot to ", plotFile)
+#ggsave(plotFile)
+png(plotFile, width = 1200, height = 800)
+plot1
+dev.off()
+if(interactiveMode){
+  ggplotly(plot1)
+}
 
 
 
 
+
+###############################################################################################################################################
 
 ################
 # Veranschauling der Fortbewegungsarten im Bezug auf einen Wegzweck
 p3<-ggplot(counts, aes(x=main_mode, y=count, fill=end_activity_type))+
   geom_bar( stat="identity",position ="stack")+
-  xlab("Mainmodes")+
+  xlab("Main mode")+
   ylab("Number of trips")+
   #theme_ipsum()+
   theme(text=element_text(size=20))+
-  ggtitle("traveling modes refenced to trip purpose")+
+  ggtitle("traveling modes per trip purpose")+
   theme(plot.title = element_text(hjust = 0.5))+
   facet_wrap(~end_activity_type, scale="free")
 
-plotFile <-paste(filePath,"/Fortbewegungsmittel_pro_Wegzweck.png",sep="")
+plotFile <-paste(outputDir,"/modes_per_trip_purpose.png",sep="")
 paste("printing plot to ", plotFile)
 png(plotFile, width = 1200, height = 800)
 p3
@@ -113,7 +188,7 @@ p4 <-ggplot(counts, aes(x=end_activity_type,y=count, fill=main_mode))+
   geom_bar( stat="identity",position ="stack")+
   ylab("Number of trips ")+
   xlab("Trip purposes")+
-  ggtitle("Trip purposes refernced to the traveling mode")+
+  ggtitle("Trip purposes per traveling mode")+
   facet_wrap(~main_mode, scale="free")+
   #theme_ipsum()+
   theme(text=element_text(size=20))+
@@ -123,7 +198,7 @@ p4 <-ggplot(counts, aes(x=end_activity_type,y=count, fill=main_mode))+
 
 
 #ggtitle("Trip purposes refernced to the traveling mode")
-plotFile <-paste(filePath,"/Weckzweck_pro_Fortbewegungsmittel_split.png",sep="")
+plotFile <-paste(outputDir,"/trip_purpose_per_mode_split.png",sep="")
 paste("printing plot to ", plotFile)
 png(plotFile, width = 1200, height = 800)
 p4
@@ -156,8 +231,8 @@ layout <- c(
 )
 figure<- p4+p5+ plot_layout(design=layout)
 #figure=p4 + inset_element(p5, left = 0.6, bottom = 1, right = 1, top = 0.6)
-ggtitle("Trip purposes refernced to the traveling mode")
-plotFile <-paste(filePath,"/Weckzweck_pro_Fortbewegungsmittel_gesamt.png",sep="")
+ggtitle("Trip purposes per traveling mode")
+plotFile <-paste(outputDir,"/trip_purpose_per_mode_all.png",sep="")
 paste("printing plot to ", plotFile)
 png(plotFile, width = 1200, height = 800)
 figure
@@ -167,20 +242,21 @@ if(interactiveMode){
 }
 
 
+
 #######
 p5 <-ggplot(counts, aes(x=end_activity_type,y=count, fill=main_mode))+
   geom_bar( stat="identity",position ="stack")+
   ylab("Number of trips")+
   xlab("Trip purposes")+
 
-  ggtitle("Trip purposes refernced to the traveling mode")+
+  ggtitle("Trip purposes referenced to the traveling mode")+
 
   theme(text=element_text(size=20))+
   theme(plot.title = element_text(hjust = 0.5))+
   theme(axis.text.x = element_text(angle = 45,hjust=1))
 
 
-plotFile <-paste(filePath,"/Weckzweck_pro_Fortbewegungsmittel_stack.png",sep="")
+plotFile <-paste(outputDir,"/modes_per_trip_purpose_stack.png",sep="")
 paste("printing plot to ", plotFile)
 png(plotFile, width = 1200, height = 800)
 p5
@@ -205,7 +281,7 @@ p2 <-ggplot(TripDataframe, aes(x=main_mode, y=stat(count), group=factor(end_acti
   theme(plot.title = element_text(hjust = 0.5))
 
 
-plotFile <-paste(filePath,"/Wegezwecke_Fortbewegungsmittel.png",sep="")
+plotFile <-paste(outputDir,"/trip_purpose_per_mode_stack.png",sep="")
 paste("printing plot to ", plotFile)
 png(plotFile, width = 1200, height = 800)
 p2
@@ -213,3 +289,4 @@ dev.off()
 if(interactiveMode){
   ggplotly(p2)
 }
+
