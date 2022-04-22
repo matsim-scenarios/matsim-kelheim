6535e1485246d1c29a90801893a94c3dcf222ee5
simei94 <simonmeinhardt94@gmail.com>
2022-04-22 13:59:22 +0200

    additional plots

--- src/main/R/LocDestAnalysis/Trippurpose-Analysis.R
+++ src/main/R/LocDestAnalysis/Trippurpose-Analysis.R
@@ -5,6 +5,7 @@ library(plotly)
 library(hrbrthemes)
 library(ggpubr)
 library(patchwork)
+
 library(RColorBrewer)
 
 require(readr)
@@ -16,7 +17,7 @@ require(readr)
 #the output_trips file should be there
 
 #runDirectory <- "D:/svn/public-svn/matsim/scenarios/countries/de/kelheim/projects/KelRide/AVServiceAreas/output/KEXI-base-case/"
-runDirectory <- "Z:/net/ils/matsim-kelheim/kelheim-case-study/KEXI-with-av/run-asv-0.7_marginal-5e-5-BAUERNSIEDLUNG/" 
+runDirectory <- "C:/Users/Simon/Documents/public-svn/matsim/scenarios/countries/de/kelheim/projects/KelRide/AVServiceAreas/output/KEXI-base-case/"
 
 interactiveMode <-FALSE
 
@@ -48,15 +49,52 @@ counts <- TripDataframe %>%
 ##################################################################################################################################################
 #######################
 
+modes <- unique(TripDataframe[c("main_mode")])
 
+#The following produces empty pngs and I do not know or how to fix this :( sm apr 22
+########################################################################################################################
+# for(mode in modes$main_mode) {
+#   print(mode)
+#   modeAnalysis <- TripDataframe %>%
+#     filter(main_mode==mode)
+#   print(length(modeAnalysis$main_mode))
+#
+#   #drt Stackbalkendiagramm
+#   plot1 <-ggplot(modeAnalysis,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
+#     geom_bar()+
+#     xlab("Trip purpose")+
+#     ylab("Traveling mode")+
+#     ggtitle("Trip purposes of trips with conventional KEXI")+
+#     scale_fill_manual(values = c("#A6CEE3", "#6EAACF", "#3687BC", "#4190AA" ,"#7EBA98" ,"#AADB84" ,"#76C15D", "#41A737", "#6D9E4C", "#C09B78", "#F88A8A", "#EE5656", "#E42123", "#EC5439", "#F6985B", "#FDB35B" ,"#FE992D", "#FF7F00")) +
+#     #theme_ipsum()+
+#     theme(text=element_text(size=20))+
+#     theme(plot.title = element_text(hjust = 0.5))+
+#     #theme(axis.text.x = element_text(angle = 45,hjust=1))+
+#     geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5))
+#
+#
+#   plot(plot1)
+#   plotFile <-paste(outputDir,"/",mode,"_trip_purposes_stacked.png",sep="")
+#   paste("printing plot to ", plotFile)
+#   #ggsave(plotFile)
+#   png(plotFile, width = 1200, height = 800)
+#   plot1
+#   dev.off()
+#   if(interactiveMode){
+#     ggplotly(plot1)
+#   }
+# }
+
+####################################################################################################################################
+#ANALYZE ALL MODES
 #analyze drt mode
-drtanalyse <- TripDataframe %>%
+drtAnalysis <- TripDataframe %>%
   filter(main_mode=="drt")
-print(length(drtanalyse$main_mode))
+print(length(drtAnalysis$main_mode))
 
 ####################
 #drt Balkendiagramm
-plot <-ggplot(drtanalyse,aes(x=end_activity_type))+
+plot <-ggplot(drtAnalysis,aes(x=end_activity_type))+
   geom_bar(fill= "steelblue")+
   xlab("Trip purpose")+
   ggtitle("Trip purposes of rides with conventional KEXI")+
@@ -79,7 +117,7 @@ if(interactiveMode){
 
 ##############
 #drt Stackbalkendiagramm
-plot1 <-ggplot(drtanalyse,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
+plot1 <-ggplot(drtAnalysis,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
   geom_bar()+
   xlab("Trip purpose")+
   ylab("Traveling mode")+
@@ -103,13 +141,13 @@ if(interactiveMode){
 }
 
 #analyze av mode
-avanalyse <- TripDataframe %>%
+avAnalysis <- TripDataframe %>%
   filter(main_mode=="av")
-print(length(drtanalyse$main_mode))
+print(length(drtAnalysis$main_mode))
 
 ####################
-#drt Balkendiagramm
-plot <-ggplot(avanalyse,aes(x=end_activity_type))+
+#av Balkendiagramm
+plot <-ggplot(avAnalysis,aes(x=end_activity_type))+
   geom_bar(fill= "steelblue")+
   xlab("Trip purpose")+
   ggtitle("Trip purposes of rides with KEXI AV")+
@@ -131,8 +169,8 @@ if(interactiveMode){
 }
 
 ##############
-#drt Stackbalkendiagramm
-plot1 <-ggplot(avanalyse,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
+#av Stackbalkendiagramm
+plot1 <-ggplot(avAnalysis,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
   geom_bar()+
   xlab("Trip purpose")+
   ylab("Traveling mode")+
@@ -146,6 +184,271 @@ plot1 <-ggplot(avanalyse,aes(x=main_mode,y=stat(count), group=factor(end_activit
 
 
 plotFile <-paste(outputDir,"/av_trip_purposes_stacked.png",sep="")
+paste("printing plot to ", plotFile)
+#ggsave(plotFile)
+png(plotFile, width = 1200, height = 800)
+plot1
+dev.off()
+if(interactiveMode){
+  ggplotly(plot1)
+}
+
+#analyze car mode
+carAnalysis <- TripDataframe %>%
+  filter(main_mode=="car")
+print(length(carAnalysis$main_mode))
+
+####################
+#car Balkendiagramm
+plot <-ggplot(carAnalysis,aes(x=end_activity_type))+
+  geom_bar(fill= "steelblue")+
+  xlab("Trip purpose")+
+  ggtitle("Trip purposes of car trips")+
+  ylab("Number of trips ")+
+  theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  #theme_ipsum()+
+  theme(text=element_text(size=20))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5),text=element_text(size=20),color="white")
+
+
+plotFile <-paste(outputDir,"/car_trip_purposes.png",sep="")
+paste("printing plot to ", plotFile)
+png(plotFile, width = 1200, height = 800)
+plot
+dev.off()
+if(interactiveMode){
+  ggplotly(plot)
+}
+
+##############
+#car Stackbalkendiagramm
+plot1 <-ggplot(carAnalysis,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
+  geom_bar()+
+  xlab("Trip purpose")+
+  ylab("Traveling mode")+
+  ggtitle("Trip purposes of car trips")+
+  scale_fill_manual(values = c("#A6CEE3", "#6EAACF", "#3687BC", "#4190AA" ,"#7EBA98" ,"#AADB84" ,"#76C15D", "#41A737", "#6D9E4C", "#C09B78", "#F88A8A", "#EE5656", "#E42123", "#EC5439", "#F6985B", "#FDB35B" ,"#FE992D", "#FF7F00")) +
+  #theme_ipsum()+
+  theme(text=element_text(size=20))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  #theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5))
+
+
+plotFile <-paste(outputDir,"/car_trip_purposes_stacked.png",sep="")
+paste("printing plot to ", plotFile)
+#ggsave(plotFile)
+png(plotFile, width = 1200, height = 800)
+plot1
+dev.off()
+if(interactiveMode){
+  ggplotly(plot1)
+}
+
+#analyze bike mode
+bikeAnalysis <- TripDataframe %>%
+  filter(main_mode=="bike")
+print(length(bikeAnalysis$main_mode))
+
+####################
+#bike Balkendiagramm
+plot <-ggplot(bikeAnalysis,aes(x=end_activity_type))+
+  geom_bar(fill= "steelblue")+
+  xlab("Trip purpose")+
+  ggtitle("Trip purposes of bike trips")+
+  ylab("Number of trips ")+
+  theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  #theme_ipsum()+
+  theme(text=element_text(size=20))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5),text=element_text(size=20),color="white")
+
+
+plotFile <-paste(outputDir,"/bike_trip_purposes.png",sep="")
+paste("printing plot to ", plotFile)
+png(plotFile, width = 1200, height = 800)
+plot
+dev.off()
+if(interactiveMode){
+  ggplotly(plot)
+}
+
+##############
+#bike Stackbalkendiagramm
+plot1 <-ggplot(bikeAnalysis,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
+  geom_bar()+
+  xlab("Trip purpose")+
+  ylab("Traveling mode")+
+  ggtitle("Trip purposes of bike trips")+
+  scale_fill_manual(values = c("#A6CEE3", "#6EAACF", "#3687BC", "#4190AA" ,"#7EBA98" ,"#AADB84" ,"#76C15D", "#41A737", "#6D9E4C", "#C09B78", "#F88A8A", "#EE5656", "#E42123", "#EC5439", "#F6985B", "#FDB35B" ,"#FE992D", "#FF7F00")) +
+  #theme_ipsum()+
+  theme(text=element_text(size=20))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  #theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5))
+
+
+plotFile <-paste(outputDir,"/bike_trip_purposes_stacked.png",sep="")
+paste("printing plot to ", plotFile)
+#ggsave(plotFile)
+png(plotFile, width = 1200, height = 800)
+plot1
+dev.off()
+if(interactiveMode){
+  ggplotly(plot1)
+}
+
+#analyze walk mode
+walkAnalysis <- TripDataframe %>%
+  filter(main_mode=="walk")
+print(length(walkAnalysis$main_mode))
+
+####################
+#walk Balkendiagramm
+plot <-ggplot(walkAnalysis,aes(x=end_activity_type))+
+  geom_bar(fill= "steelblue")+
+  xlab("Trip purpose")+
+  ggtitle("Trip purposes of walk trips")+
+  ylab("Number of trips ")+
+  theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  #theme_ipsum()+
+  theme(text=element_text(size=20))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5),text=element_text(size=20),color="white")
+
+
+plotFile <-paste(outputDir,"/walk_trip_purposes.png",sep="")
+paste("printing plot to ", plotFile)
+png(plotFile, width = 1200, height = 800)
+plot
+dev.off()
+if(interactiveMode){
+  ggplotly(plot)
+}
+
+##############
+#walk Stackbalkendiagramm
+plot1 <-ggplot(walkAnalysis,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
+  geom_bar()+
+  xlab("Trip purpose")+
+  ylab("Traveling mode")+
+  ggtitle("Trip purposes of walk trips")+
+  scale_fill_manual(values = c("#A6CEE3", "#6EAACF", "#3687BC", "#4190AA" ,"#7EBA98" ,"#AADB84" ,"#76C15D", "#41A737", "#6D9E4C", "#C09B78", "#F88A8A", "#EE5656", "#E42123", "#EC5439", "#F6985B", "#FDB35B" ,"#FE992D", "#FF7F00")) +
+  #theme_ipsum()+
+  theme(text=element_text(size=20))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  #theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5))
+
+
+plotFile <-paste(outputDir,"/walk_trip_purposes_stacked.png",sep="")
+paste("printing plot to ", plotFile)
+#ggsave(plotFile)
+png(plotFile, width = 1200, height = 800)
+plot1
+dev.off()
+if(interactiveMode){
+  ggplotly(plot1)
+}
+
+#analyze pt mode
+ptAnalysis <- TripDataframe %>%
+  filter(main_mode=="pt")
+print(length(ptAnalysis$main_mode))
+
+####################
+#pt Balkendiagramm
+plot <-ggplot(ptAnalysis,aes(x=end_activity_type))+
+  geom_bar(fill= "steelblue")+
+  xlab("Trip purpose")+
+  ggtitle("Trip purposes of pt trips")+
+  ylab("Number of trips ")+
+  theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  #theme_ipsum()+
+  theme(text=element_text(size=20))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5),text=element_text(size=20),color="white")
+
+
+plotFile <-paste(outputDir,"/pt_trip_purposes.png",sep="")
+paste("printing plot to ", plotFile)
+png(plotFile, width = 1200, height = 800)
+plot
+dev.off()
+if(interactiveMode){
+  ggplotly(plot)
+}
+
+##############
+#pt Stackbalkendiagramm
+plot1 <-ggplot(ptAnalysis,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
+  geom_bar()+
+  xlab("Trip purpose")+
+  ylab("Traveling mode")+
+  ggtitle("Trip purposes of pt trips")+
+  scale_fill_manual(values = c("#A6CEE3", "#6EAACF", "#3687BC", "#4190AA" ,"#7EBA98" ,"#AADB84" ,"#76C15D", "#41A737", "#6D9E4C", "#C09B78", "#F88A8A", "#EE5656", "#E42123", "#EC5439", "#F6985B", "#FDB35B" ,"#FE992D", "#FF7F00")) +
+  #theme_ipsum()+
+  theme(text=element_text(size=20))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  #theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5))
+
+
+plotFile <-paste(outputDir,"/pt_trip_purposes_stacked.png",sep="")
+paste("printing plot to ", plotFile)
+#ggsave(plotFile)
+png(plotFile, width = 1200, height = 800)
+plot1
+dev.off()
+if(interactiveMode){
+  ggplotly(plot1)
+}
+
+#analyze ride mode
+rideAnalysis <- TripDataframe %>%
+  filter(main_mode=="ride")
+print(length(rideAnalysis$main_mode))
+
+####################
+#ride Balkendiagramm
+plot <-ggplot(rideAnalysis,aes(x=end_activity_type))+
+  geom_bar(fill= "steelblue")+
+  xlab("Trip purpose")+
+  ggtitle("Trip purposes of ride trips")+
+  ylab("Number of trips ")+
+  theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  #theme_ipsum()+
+  theme(text=element_text(size=20))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5),text=element_text(size=20),color="white")
+
+
+plotFile <-paste(outputDir,"/ride_trip_purposes.png",sep="")
+paste("printing plot to ", plotFile)
+png(plotFile, width = 1200, height = 800)
+plot
+dev.off()
+if(interactiveMode){
+  ggplotly(plot)
+}
+
+##############
+#ride Stackbalkendiagramm
+plot1 <-ggplot(rideAnalysis,aes(x=main_mode,y=stat(count), group=factor(end_activity_type), fill=factor(end_activity_type)))+
+  geom_bar()+
+  xlab("Trip purpose")+
+  ylab("Traveling mode")+
+  ggtitle("Trip purposes of ride trips")+
+  scale_fill_manual(values = c("#A6CEE3", "#6EAACF", "#3687BC", "#4190AA" ,"#7EBA98" ,"#AADB84" ,"#76C15D", "#41A737", "#6D9E4C", "#C09B78", "#F88A8A", "#EE5656", "#E42123", "#EC5439", "#F6985B", "#FDB35B" ,"#FE992D", "#FF7F00")) +
+  #theme_ipsum()+
+  theme(text=element_text(size=20))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  #theme(axis.text.x = element_text(angle = 45,hjust=1))+
+  geom_text(aes(label=stat(count)),stat="count",position = position_stack(vjust = 0.5))
+
+
+plotFile <-paste(outputDir,"/ride_trip_purposes_stacked.png",sep="")
 paste("printing plot to ", plotFile)
 #ggsave(plotFile)
 png(plotFile, width = 1200, height = 800)
