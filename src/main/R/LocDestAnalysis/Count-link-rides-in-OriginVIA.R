bc482a6fb6363461488dc238e66508eea3c3513f
simei94 <meinhardt@vsp.tu-berlin.de>
2023-03-09 18:03:11 +0100

    updated scripts to keep them runnable

--- src/main/R/LocDestAnalysis/Count-link-rides-in-OriginVIA.R
+++ src/main/R/LocDestAnalysis/Count-link-rides-in-OriginVIA.R
@@ -6,12 +6,14 @@ library(lubridate)
 ######################
 # dies sind die Variablen, die man für den eigenen Gebrauch anpassen muss
 programPath <- "C:/Users/Simon/Documents/VSP-Projects/matsim-kelheim/src/main/R/LocDestAnalysis" # hier sollen die Funktionen neareststop.R und countdrivenstops.R liegen
-filePath <- "C:/Users/Simon/Documents/shared-svn/projects/KelRide/data/KEXI" # hier sollen die beiden folgenden Files liegen
-originfilename <- "VIA_Rides_202106_202201.csv" # Filename der Realdaten
+filePath <- "C:/Users/Simon/Desktop/wd/2023-03-06" # hier sollen die beiden folgenden Files liegen
+#filePath <- "C:/Users/Simon/Documents/shared-svn/projects/KelRide/data/KEXI" # hier sollen die beiden folgenden Files liegen
+#originfilename <- "VIA_Rides_202106_202201.csv" # Filename der Realdaten
+originfilename <- "20230301_AV-Fahrtanfragen-Dec-Mar_not-completed.csv" # Filename der Realdaten
 haltestellenFile <- "KEXI_Haltestellen_Liste_Kelheim.csv" # Filename der Haltestellenliste
-startdate <- "2022-01-27"  # wenn das ganze File ausgewertet werden soll, schreibe hier -1 rein
+startdate <- "-1"  # wenn das ganze File ausgewertet werden soll, schreibe hier -1 rein
 tage <- 1 # Anzahl an Tagen die ausgewertet werden sollen
-nameAnalyseFile <- paste("typicalDays/", startdate , "_Via-Origin-drt-count-Analysis-KEXI.csv") # Filename des Analyseoutputfiles, in tsv
+nameAnalyseFile <- paste0(startdate,"_stop2stop_",originfilename) # Filename des Analyseoutputfiles, in tsv
 csvfilename <- "stop2stopridesVIA.csv"
 setwd(programPath)
 source("neareststop.R")
@@ -23,7 +25,9 @@ setwd(filePath)
 # Daten von VIA einlesen
 viaDataframe <- read.csv(originfilename, stringsAsFactors = FALSE, header = TRUE, encoding = "UTF-8", sep= ";")
 
-viaDataframe <- viaDataframe %>% mutate(Actual.DO.time = ymd_hms(Actual.DO.time))
+viaDataframe <- viaDataframe %>% mutate(Angefragte.Einstiegszeit = ymd_hms(Angefragte.Einstiegszeit)) %>%
+  rename("Start.Laengengrad" = "Start.Längengrad",
+         "Zielort.Laengengrad" = "Zielort.Längengrad")
 
 if (startdate!=-1){
   d <- interval(ymd(startdate),ymd(startdate)+days(x=tage))
@@ -47,13 +51,13 @@ if(("fromstopID" %in% colnames(viaDataframe))==FALSE){
 
   viaDataframe <- viaDataframe  %>%
     rowwise() %>%
-    #mutate(fromstopID=as.integer(neareststop(Origin.Longitude, Origin.latitude,"Stop.ID")))
-    mutate(fromstopID=as.integer(neareststop(Origin.Longitude, Origin.latitude,"Haltestellen.Nr.")))
+    mutate(fromstopID=as.integer(neareststop(Start.Laengengrad, Start.Breitengrad,"Haltestellen-Nr.")))
+    # mutate(fromstopID=as.integer(neareststop(Origin.Longitude, Origin.latitude,"Haltestellen.Nr.")))
 
   viaDataframe <- viaDataframe  %>%
     rowwise() %>%
-    #mutate(tostop_ID = as.integer(neareststop(Destination.Longitude, Destination.latitude,"Stop.ID")))
-    mutate(tostop_ID=as.integer(neareststop(Destination.Longitude, Destination.latitude,"Haltestellen.Nr.")))
+    mutate(tostop_ID = as.integer(neareststop(Zielort.Laengengrad, Zielort.Breitengrad,"Haltestellen-Nr.")))
+    # mutate(tostop_ID=as.integer(neareststop(Destination.Longitude, Destination.latitude,"Haltestellen.Nr.")))
 
   setwd(filePath)
 
