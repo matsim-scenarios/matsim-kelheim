e2a1a177fc972d5b81397674fa8966630b52ad12
Jakob Rehmann <jakob.rehmann@gmail.com>
2023-05-08 18:48:11 +0200

    created markdown for winner/loser analysis for kelheim

--- src/main/R/score_diff.R
+++ src/main/R/score_diff.R
@@ -2,85 +2,44 @@ library(sf)
 library(tmap)
 library(tidyverse)
 library(units)
-devtools::install_github("matsim-vsp/matsim-r", ref="jr_bug_fixes", force = TRUE)
+# devtools::install_github("matsim-vsp/matsim-r", ref="jr_bug_fixes", force = TRUE)
+devtools::load_all("~/git/matsim-r")
 library(matsim)
-# devtools::load_all("~/git/matsim-r")
-
-# load utils
-source("~/git/matsim-kelheim/src/main/R/utils_jr.R")
 
 # global options
 tmap_mode("view")
 tmap_options(check.and.fix = TRUE)
 
 # read in base and policy cases
-baseCaseDirectory <- "~/git/net/ils/matsim-kelheim/calibration/runs/052"
-policyCaseDirectory <- "~/git/net/ils/matsim-kelheim/kelheim-case-study/v2.0/KEXI-with-av/output-ASC-0.15-dist-0.00006-5_av-seed5678-BAUERNSIEDLUNG"
-baseCaseDirectory <- "/Users/jakob/base"
-policyCaseDirectory <- "/Users/jakob/policy"
+base_path <- "~/git/runs-svn/KelRide/matsim-kelheim-v2.0/1-Base-Case-With-Conventional-KEXI/output-ASC-0.15-dist-0.00006-intermodal-kexi-seed_1111/"
+policy_path <- "~/git/runs-svn/KelRide/matsim-kelheim-v2.0/2-AV-Service-Area-And-Fleet-Size/runs/output-ASC-0.15-dist-0.00006-2_av-seed1111-CORE/"
 
-basePersons <- readPersonsTable(baseCaseDirectory)
-policyPersons <- readPersonsTable(policyCaseDirectory)
+persons_base <- readPersonsTable(base_path)
+persons_policy <- readPersonsTable(policy_path)
+
+trips_base <- readTripsTable(base_path)
+trips_policy <- readTripsTable(policy_path)
 
 shp <- st_read("~/git/matsim-kelheim/scenarios/input/shp/dilutionArea.shx") %>% 
   st_set_crs(25832)
 
-joined <- join_base_and_policy(basePersons,policyPersons, shp)
+persons_joined <- join_base_and_policy_persons(persons_base, persons_policy)
 
-joined_hex <- create_hex_grid(joined, shp)
+persons_joined_sf <- transform_persons_sf(persons_joined, filter_shp = shp, first_act_type_filter = "home")
 
-# pop density
-tm_shape(joined_hex) +
-  tm_polygons(col = "cnt", alpha = 0.75)
+joined_hex <- persons_attributes_on_hex_grid(persons_joined_sf, shp)
 
-#income
-ggplot(joined) +
-  geom_density(aes(income))
+joined_centroids <- joined_hex %>% st_centroid()
 
-tm_shape(joined_hex) +
-  tm_polygons(col = "income", alpha = 0.75)
-
-#carAvail
-tm_shape(joined_hex) +
-  tm_polygons(col = "carAvail", alpha = 0.75)
-
-#pt
-tm_shape(joined_hex) +
-  tm_polygons(col = "sim_ptAbo", alpha = 0.75)
-
-
-#income by carAvail
-ggplot(joined) +
-  geom_density(aes(income, col = carAvail))
-
-#income by pt
-ggplot(joined) +
-  geom_density(aes(income, col = sim_ptAbo))
-
-tm_shape(joined) +
-  tm_dots(col = "income", size = 0.001)
-
-
-# score difference plot
-
-ggplot(joined) + 
-  geom_boxplot(aes(score_diff))
-
-
-tm_shape(joined_hex) +
-  tm_polygons(col = "score_diff", alpha = 0.7)
-
-
-to_save <- joined_hex %>% st_centroid()
-
-tm_shape(to_save)+
+tm_shape(joined_centroids)+
   tm_bubbles(col = "score_diff", size = "pop_density")
 
-st_write(to_save, "~/git/simwrapper-example-project/data/kelheim-ex/points.shp")
 
-st_write(joined_hex, "~/git/simwrapper-example-project/data/kelheim-ex/points2.shp")
+st_write(joined_centroids, "~/git/simwrapper-example-project/data/kelheim-ex/score_diff_points.shp")
 
-st_write(to_save %>% st_buffer(100), "~/git/simwrapper-example-project/data/kelheim-ex/points_poly.shp")
+st_write(joined_hex, "~/git/simwrapper-example-project/data/kelheim-ex/score_diff_hex.shp")
+
+st_write(joined_centroids %>% st_buffer(1000), "~/git/simwrapper-example-project/data/kelheim-ex/score_diff_polygon.shp")
 
 
 
@@ -115,6 +74,39 @@ ggplot(joined) +
 ggplot(joined) +
   geom_smooth(aes(income,score_diff, col = sim_ptAbo)) 
 
+
+### old
+
+# pop density
+tm_shape(joined_hex) +
+  tm_polygons(col = "cnt", alpha = 0.75)
+
+#income
+ggplot(joined) +
+  geom_density(aes(income))
+
+tm_shape(joined_hex) +
+  tm_polygons(col = "income", alpha = 0.75)
+
+#carAvail
+tm_shape(joined_hex) +
+  tm_polygons(col = "carAvail", alpha = 0.75)
+
+#pt
+tm_shape(joined_hex) +
+  tm_polygons(col = "sim_ptAbo", alpha = 0.75)
+
+
+#income by carAvail
+ggplot(joined) +
+  geom_density(aes(income, col = carAvail))
+
+#income by pt
+ggplot(joined) +
+  geom_density(aes(income, col = sim_ptAbo))
+
+tm_shape(joined) +
+  tm_dots(col = "income", size = 0.001)
 
 
 # dist to center
