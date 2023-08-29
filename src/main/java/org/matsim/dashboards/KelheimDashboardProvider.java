11ea683dcd67b7a8d051036f0fd62ada8c162ea6
rakow <rakow@vsp.tu-berlin.de>
2023-08-29 10:37:37 +0200

    add route validation data

--- src/main/java/org/matsim/dashboards/KelheimDashboardProvider.java
+++ src/main/java/org/matsim/dashboards/KelheimDashboardProvider.java
@@ -1,9 +1,12 @@
 package org.matsim.dashboards;
 
+import org.matsim.application.ApplicationUtils;
 import org.matsim.core.config.Config;
+import org.matsim.run.RunKelheimScenario;
 import org.matsim.simwrapper.Dashboard;
 import org.matsim.simwrapper.DashboardProvider;
 import org.matsim.simwrapper.SimWrapper;
+import org.matsim.simwrapper.dashboard.TravelTimeComparisonDashboard;
 import org.matsim.simwrapper.dashboard.TripDashboard;
 
 import java.util.List;
@@ -20,8 +23,10 @@ public class KelheimDashboardProvider implements DashboardProvider {
 		TripDashboard trips = new TripDashboard("kelheim_mode_share.csv", "kelheim_mode_share_per_dist.csv", null);
 
 		trips.setAnalysisArgs("--dist-groups", "0,1000,2000,5000,10000,20000");
-
-		return List.of(trips);
+		return List.of(
+			trips,
+			new TravelTimeComparisonDashboard(ApplicationUtils.resolve(config.getContext(), "kelheim-v" + RunKelheimScenario.VERSION + "-routes-ref.csv.gz"))
+		);
 	}
 
 }
