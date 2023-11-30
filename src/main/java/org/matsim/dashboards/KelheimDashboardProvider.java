2b589762d1e76822a802c282e98f7dc75eab5c5b
rakow <rakow@vsp.tu-berlin.de>
2023-11-30 19:25:26 +0100

    closes #68

--- src/main/java/org/matsim/dashboards/KelheimDashboardProvider.java
+++ src/main/java/org/matsim/dashboards/KelheimDashboardProvider.java
@@ -2,7 +2,6 @@ package org.matsim.dashboards;
 
 import org.matsim.core.config.Config;
 import org.matsim.core.utils.io.IOUtils;
-import org.matsim.run.RunKelheimScenario;
 import org.matsim.simwrapper.Dashboard;
 import org.matsim.simwrapper.DashboardProvider;
 import org.matsim.simwrapper.SimWrapper;
@@ -25,7 +24,7 @@ public class KelheimDashboardProvider implements DashboardProvider {
 		trips.setAnalysisArgs("--dist-groups", "0,1000,2000,5000,10000,20000");
 		return List.of(
 			trips,
-			new TravelTimeComparisonDashboard(IOUtils.resolveFileOrResource( "kelheim-v" + RunKelheimScenario.VERSION + "-routes-ref.csv.gz").toString())
+			new TravelTimeComparisonDashboard(IOUtils.resolveFileOrResource( "kelheim-v3.0-routes-ref.csv.gz").toString())
 		);
 	}
 
