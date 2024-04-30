ebcc722c501c643680ef04a9159b97b525fa7cb9
tschlenther <schlenther@vsp.tu-berlin.de>
2024-04-30 13:28:47 +0200

    update emission dashboard + post-process runner

--- src/main/java/org/matsim/analysis/emissions/KelheimEmissionsDashboard.java
+++ src/main/java/org/matsim/analysis/emissions/KelheimEmissionsDashboard.java
@@ -35,19 +35,37 @@ import org.matsim.simwrapper.viz.XYTime;
  * which has specific network and vehicle type attributes.
  */
 public class KelheimEmissionsDashboard implements Dashboard{
+	private final String pathToCsvBase;
+
 	public KelheimEmissionsDashboard() {
+		this.pathToCsvBase = null;
+	}
+
+	public KelheimEmissionsDashboard(String pathToBaseRun) {
+		if (!pathToBaseRun.endsWith("/")) {
+			pathToBaseRun += "/";
+		}
+		this.pathToCsvBase = pathToBaseRun + "analysis/emissions/emissions_per_link_per_m.csv";
 	}
 
 	/**
 	 * Produces the dashboard.
 	 */
 	public void configure(Header header, Layout layout) {
+
 		header.title = "Emissions";
 		header.description = "Shows the emissions footprint and spatial distribution.";
+
+		String linkDescription = "Displays the emissions for each link per meter. Be aware that emission values are provided in the simulation sample size!";
+		if (pathToCsvBase != null){
+			linkDescription += String.format("\n Base is %s", pathToCsvBase);
+		}
+		String finalLinkDescription = linkDescription;
+
 		layout.row("links")
 			.el(Table.class, (viz, data) -> {
 				viz.title = "Emissions";
-				viz.description = "by pollutant. Values are scaled from the simulation sample size to 100%.";
+				viz.description = "by pollutant. Total values are scaled from the simulation sample size to 100%.";
 				viz.dataset = data.compute(KelheimOfflineAirPollutionAnalysisByEngineInformation.class, "emissions_total.csv", new String[0]);
 				viz.enableFilter = false;
 				viz.showAllRows = true;
@@ -55,12 +73,15 @@ public class KelheimEmissionsDashboard implements Dashboard{
 			})
 			.el(Links.class, (viz, data) -> {
 				viz.title = "Emissions per Link per Meter";
-				viz.description = "Displays the emissions for each link per meter. Be aware that values are shown in the simulation sample size.";
+				viz.description = finalLinkDescription;
 				viz.height = 12.0;
 				viz.datasets.csvFile = data.compute(KelheimOfflineAirPollutionAnalysisByEngineInformation.class, "emissions_per_link_per_m.csv", new String[0]);
+				viz.datasets.csvBase = this.pathToCsvBase;
 				viz.network = data.compute(CreateGeoJsonNetwork.class, "network.geojson", new String[0]);
 				viz.display.color.columnName = "CO2_TOTAL [g/m]";
 				viz.display.color.dataset = "csvFile";
+				//TODO how to set color ramp??
+//				viz.display.color.setColorRamp(Plotly.ColorScheme.RdBu, 5, true);
 				viz.display.width.scaleFactor = 100;
 				viz.display.width.columnName = "CO2_TOTAL [g/m]";
 				viz.display.width.dataset = "csvFile";
@@ -69,14 +90,14 @@ public class KelheimEmissionsDashboard implements Dashboard{
 		});
 		layout.row("second").el(XYTime.class, (viz, data) -> {
 			viz.title = "CO₂ Emissions";
-			viz.description = "per day. Be aware that values are shown in the simulation sample size.";
+			viz.description = "per day. Be aware that CO2 values are provided in the simulation sample size!";
 			viz.height = 12.0;
 			viz.file = data.compute(KelheimOfflineAirPollutionAnalysisByEngineInformation.class, "emissions_grid_per_day.xyt.csv", new String[0]);
 		});
 		layout.row("third")
 			.el(XYTime.class, (viz, data) -> {
 				viz.title = "CO₂ Emissions";
-				viz.description = "per hour. Be aware that values are shown in the simulation sample size.";
+				viz.description = "per hour. Be aware that CO2 values are provided in the simulation sample size!";
 				viz.height = 12.;
 				viz.file = data.compute(KelheimOfflineAirPollutionAnalysisByEngineInformation.class, "emissions_grid_per_hour.csv");
 			});
