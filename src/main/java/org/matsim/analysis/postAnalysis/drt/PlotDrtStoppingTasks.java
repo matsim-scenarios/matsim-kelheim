3d48fa6c7ff6d1bb59ebe910ab321b9f2d79f9f4
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2024-06-03 11:28:28 +0200

    Create PlotDrtStoppingTasks.java (#77)
    * Create PlotDrtStoppingTasks.java
    
    * checkstyle
    
    ---------
    
    Co-authored-by: schlenther <schlenther@vsp.tu-berlin.de>

---
+++ src/main/java/org/matsim/analysis/postAnalysis/drt/PlotDrtStoppingTasks.java
@@ -0,0 +1,20 @@
+package org.matsim.analysis.postAnalysis.drt;
+
+import org.matsim.contrib.drt.analysis.afterSimAnalysis.DrtVehicleStoppingTaskWriter;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * Plot DRT stopping task (idle and pickup/drop-off stops) on the map, with start time and end time.
+ * Please specify the output directory of the DRT in the input argument.
+ */
+public final class PlotDrtStoppingTasks {
+
+	private PlotDrtStoppingTasks() {}
+
+	public static void main(String[] args) throws IOException {
+		new DrtVehicleStoppingTaskWriter(Path.of(args[0])).run();
+	}
+
+}
