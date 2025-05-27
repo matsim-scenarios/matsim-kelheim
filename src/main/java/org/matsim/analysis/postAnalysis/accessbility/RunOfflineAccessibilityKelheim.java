ac82a1bb2995c45e20a5e004249627300412c8c8
Jakob Rehmann <jakob.rehmann@gmail.com>
2025-05-27 12:19:36 +0200

    cleaned up kelheim offline run to fix checkstyle issues

--- src/main/java/org/matsim/analysis/postAnalysis/accessbility/RunOfflineAccessibilityKelheim.java
+++ src/main/java/org/matsim/analysis/postAnalysis/accessbility/RunOfflineAccessibilityKelheim.java
@@ -6,6 +6,8 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.geotools.api.referencing.FactoryException;
 import org.geotools.api.referencing.crs.CoordinateReferenceSystem;
 import org.geotools.api.referencing.operation.MathTransform;
@@ -35,6 +37,7 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigGroup;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.FacilitiesConfigGroup;
+import org.matsim.core.config.groups.RoutingConfigGroup;
 import org.matsim.core.config.groups.ScoringConfigGroup;
 import org.matsim.core.controler.*;
 import org.matsim.core.scenario.MutableScenario;
@@ -58,10 +61,21 @@ import java.util.List;
 import java.util.Set;
 
 
+/**
+ * Run this class to calculate accessibility for the Kelheim scenario for different modes (including DRT). This class is meant to be run after
+ * a simulation (offline).
+ */
 public class RunOfflineAccessibilityKelheim {
 
-	static String OUTPUT_DIR = "../public-svn/matsim/scenarios/countries/de/kelheim/kelheim-accessibility-dashboard/kexi-seed-1-ASC-2.45/";
+	private static final Logger log = LogManager.getLogger(RunOfflineAccessibilityKelheim.class);
 
+	private static final String OUTPUT_DIR = "../public-svn/matsim/scenarios/countries/de/kelheim/drtAccessibility/kelheim-scratch/";
+
+
+	protected RunOfflineAccessibilityKelheim() {
+		// should not be instantiated
+		throw new UnsupportedOperationException();
+	}
 
 	public static void main(String[] args) throws FactoryException, TransformException {
 
@@ -76,40 +90,197 @@ public class RunOfflineAccessibilityKelheim {
 		Coordinate leftBottomWgs84 = new Coordinate(11.574, 48.584);
 		Coordinate topRightWgs84 = new Coordinate(12.095, 48.994);
 		String mapCenterString = (leftBottomWgs84.x + topRightWgs84.x) / 2 + "," + (leftBottomWgs84.y + topRightWgs84.y) / 2;
-		Coordinate leftBottom = transformCoordinate(CRS.decode("EPSG:4326",true),CRS.decode("EPSG:25832"), leftBottomWgs84);
-		Coordinate rightTop = transformCoordinate(CRS.decode("EPSG:4326",true), CRS.decode("EPSG:25832"), topRightWgs84);
+		Coordinate leftBottom = transformCoordinate(CRS.decode("EPSG:4326", true), CRS.decode("EPSG:25832"), leftBottomWgs84);
+		Coordinate rightTop = transformCoordinate(CRS.decode("EPSG:4326", true), CRS.decode("EPSG:25832"), topRightWgs84);
 		accConfig.setBoundingBoxLeft(leftBottom.x);
 		accConfig.setBoundingBoxBottom(leftBottom.y);
 		accConfig.setBoundingBoxRight(rightTop.x);
 		accConfig.setBoundingBoxTop(rightTop.y);
-		accConfig.setTileSize_m(250);//250
+		accConfig.setTileSize_m(250);
 
-		List<Double> timesHour = List.of(8.0);
+//		List<Double> timesHour = List.of(8.5, 9.5, 10.5);
 //		List<Double> timesHour = List.of(8.0, 12.0, 16.0);
-//		List<Double> timesHour = List.of(6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0);
+//		List<Double> timesHour = List.of(0.0,6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0);
+		List<Double> timesHour = List.of(8.0);
 		List<Double> timesSeconds = timesHour.stream().map(t -> t * 60 * 60).toList();
 
 		accConfig.setTimeOfDay(timesSeconds);
-		List<Modes4Accessibility> accModes = List.of(Modes4Accessibility.car, Modes4Accessibility.pt, Modes4Accessibility.estimatedDrt, Modes4Accessibility.walk, Modes4Accessibility.bike, Modes4Accessibility.teleportedWalk);
+//		List<Modes4Accessibility> accModes = List.of(Modes4Accessibility.car, Modes4Accessibility.freespeed, Modes4Accessibility.pt, Modes4Accessibility.estimatedDrt, Modes4Accessibility.walk, Modes4Accessibility.teleportedWalk);
+		List<Modes4Accessibility> accModes = List.of(Modes4Accessibility.pt);
 
-		for(Modes4Accessibility mode : Modes4Accessibility.values()) {
+		for (Modes4Accessibility mode : Modes4Accessibility.values()) {
 			accConfig.setComputingAccessibilityForMode(mode, accModes.contains(mode));
 		}
-
 
 		// Part 1: Generate Parameters for Estimator
 		// Part 2: Calculate Accessibility
 
-		EstimatorParameters estimatorParameters = step1_generateParams();
-		step2_calculateAccessibility(estimatorParameters, relevantPois, accConfig);
+//		EstimatorParameters estimatorParameters = step1_generateParams();
+//		step2_calculateAccessibility(estimatorParameters, relevantPois, accConfig);
 
 		// Part 3: Create Dashboard
-		step3_createDashboard(relevantPois, accModes, mapCenterString);
-
+		step3CreateDashboard(relevantPois, accModes, mapCenterString);
 
 	}
 
-	private static void step3_createDashboard(List<String> relevantPois, List<Modes4Accessibility> accModes, String mapCenterString) {
+
+
+
+
+
+
+	private static EstimatorParameters step1GenerateParams() {
+
+		DoubleList inVehicleTravelTime = new DoubleArrayList();
+		DoubleList directTravelDistance_m = new DoubleArrayList();
+
+		double n = 0.;
+		double waitTimeSum = 0.;
+
+		Path filePath = Path.of(OUTPUT_DIR + "kexi-seed1-ASC-2.45.output_drt_legs_drt.csv");
+		try (CSVParser parser = new CSVParser(new BufferedReader(new InputStreamReader(Files.newInputStream(filePath))),
+			CSVFormat.DEFAULT.withDelimiter(';').withFirstRecordAsHeader())) {
+
+			for (CSVRecord csvRecord : parser) {
+				n++;
+				waitTimeSum += Double.parseDouble(csvRecord.get("waitTime"));
+				inVehicleTravelTime.add(Double.parseDouble(csvRecord.get("inVehicleTravelTime")));
+				directTravelDistance_m.add(Double.parseDouble(csvRecord.get("directTravelDistance_m")));
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		double waitTime_s = waitTimeSum / n;
+		log.info("Wait time (seconds):" + waitTime_s);
+		log.info("Wait time (minutes):" + waitTime_s/60);
+
+		SimpleRegression regression = new SimpleRegression(true);
+
+		for (int i = 0; i < n; i++) {
+			regression.addData(directTravelDistance_m.getDouble(i), inVehicleTravelTime.getDouble(i));
+		}
+
+		// Get the intercept and slope
+		double intercept = regression.getIntercept();
+		double slope = regression.getSlope();
+
+
+		log.info("Intercept: " + intercept);
+		log.info("Slope: " + slope);
+		log.info("R2: " + regression.getRSquare());
+
+		return new EstimatorParameters(intercept, slope, waitTime_s);
+	}
+
+
+
+	private static void step2CalculateAccessibility(EstimatorParameters estimatorParameters, List<String> relevantPois, ConfigGroup accConfig) {
+
+		// input files
+		String stopsFile = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/kelheim/kelheim-drt-accessibility-JB-master/input/drt-stops-land.xml";
+		String poiFile = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/kelheim/kelheim-drt-accessibility-JB-master/input/pois_complete.csv";
+
+		String eventsFile = ApplicationUtils.matchInput("output_events.xml.gz", Path.of(OUTPUT_DIR)).toString();
+		String networkFile = ApplicationUtils.matchInput("output_network.xml.gz", Path.of(OUTPUT_DIR)).toString();
+		String transportScheduleFile = ApplicationUtils.matchInput("output_transitSchedule.xml.gz", Path.of(OUTPUT_DIR)).toString();
+
+
+		// CONFIG
+		//global
+		final Config config = ConfigUtils.createConfig();
+
+		config.routing().setAccessEgressType(RoutingConfigGroup.AccessEgressType.accessEgressModeToLink);
+
+
+		config.controller().setLastIteration(0);
+//		config.controller().setOutputDirectory(utils.getOutputDirectory());
+		config.controller().setOutputDirectory(OUTPUT_DIR);
+		config.controller().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.overwriteExistingFiles);
+
+		config.routing().setRoutingRandomness(0.);
+
+		config.global().setCoordinateSystem("EPSG:25832");
+
+		config.network().setInputFile(networkFile);
+
+
+		// transit
+		config.transit().setTransitScheduleFile(transportScheduleFile);
+		config.transitRouter().setSearchRadius(50_000);
+		config.transitRouter().setExtensionRadius(50_000);
+
+		// change walk speed to match kelheim scenario
+		config.routing().getTeleportedModeParams().get(TransportMode.walk).setTeleportedModeSpeed(3.8 / 3.6);
+
+		// change scoring default to match kelheim scenario
+		ScoringConfigGroup.ModeParams drtParams = new ScoringConfigGroup.ModeParams(TransportMode.drt);
+		drtParams.setMarginalUtilityOfDistance(-2.5E-4);
+		drtParams.setMarginalUtilityOfTraveling(0.0);
+		config.scoring().addModeParams(drtParams);
+
+		ScoringConfigGroup.ModeParams walkParams = config.scoring().getModes().get(TransportMode.walk);
+		walkParams.setMarginalUtilityOfTraveling(0.0);
+		config.scoring().addModeParams(walkParams);
+
+		// accessibility config
+
+		config.addModule(accConfig);
+
+		// drt config
+		ConfigUtils.addOrGetModule( config, DvrpConfigGroup.class );
+
+		DrtConfigGroup drtConfigGroup = new DrtConfigGroup();
+		drtConfigGroup.operationalScheme = DrtConfigGroup.OperationalScheme.stopbased;
+		drtConfigGroup.transitStopFile = stopsFile;
+
+		drtConfigGroup.addOrGetDrtOptimizationConstraintsParams().addOrGetDefaultDrtOptimizationConstraintsSet().maxWalkDistance = 100000.;
+
+		MultiModeDrtConfigGroup multiModeDrtConfigGroup = new MultiModeDrtConfigGroup();
+		multiModeDrtConfigGroup.addParameterSet(drtConfigGroup);
+		config.addModule(multiModeDrtConfigGroup);
+		config.addModule(drtConfigGroup);
+
+		// SCENARIO
+
+		MutableScenario scenario = (MutableScenario) ScenarioUtils.loadScenario(config);
+
+		for (Link link : scenario.getNetwork().getLinks().values()) {
+			if (!link.getId().toString().startsWith("pt_")) {
+				Set<String> modes = new HashSet<>(link.getAllowedModes());
+				modes.add(TransportMode.walk);
+				modes.add(TransportMode.bike);
+				link.setAllowedModes(modes);
+			}
+
+		}
+
+
+
+		// add pois to scenario as facilities
+		readPoiCsv(scenario.getActivityFacilities(), poiFile);
+
+
+		AccessibilityFromEvents.Builder builder = new AccessibilityFromEvents.Builder(scenario, eventsFile, relevantPois);
+
+		// configure DRT Estimator with wait time, and ride time parameters
+		DrtEstimator drtEstimator = new DirectTripDistanceBasedDrtEstimator.Builder()
+			.setWaitingTimeEstimator(new ConstantWaitingTimeEstimator(estimatorParameters.getWaitTime()))
+			.setWaitingTimeDistributionGenerator(new NoDistribution())
+			.setRideDurationEstimator(new ConstantRideDurationEstimator(estimatorParameters.getSlope(), estimatorParameters.getIntercept()))
+			.setRideDurationDistributionGenerator(new NoDistribution())
+			.build();
+
+
+
+		builder.addDrtEstimator(drtEstimator);
+
+
+		builder.build().run();
+
+	}
+
+	private static void step3CreateDashboard(List<String> relevantPois, List<Modes4Accessibility> accModes, String mapCenterString) {
 
 		final Config config = ConfigUtils.createConfig();
 		config.controller().setOutputDirectory(OUTPUT_DIR);
@@ -147,176 +318,6 @@ public class RunOfflineAccessibilityKelheim {
 
 	}
 
-	public static class EstimatorParameters {
-		private final double intercept;
-		private final double slope;
-		private final double waitTime;
-
-		public EstimatorParameters(double value1, double value2, double value3) {
-			this.intercept = value1;
-			this.slope = value2;
-			this.waitTime = value3;
-		}
-
-		public double getIntercept() {
-			return intercept;
-		}
-
-		public double getSlope() {
-			return slope;
-		}
-
-		public double getWaitTime() {
-			return waitTime;
-		}
-	}
-
-
-	private static EstimatorParameters step1_generateParams() {
-
-		DoubleList inVehicleTravelTime = new DoubleArrayList();
-		DoubleList directTravelDistance_m = new DoubleArrayList();
-
-		double n = 0.;
-		double waitTimeSum = 0.;
-
-		Path filePath = Path.of(OUTPUT_DIR + "kexi-seed1-ASC-2.45.output_drt_legs_drt.csv");
-		try (CSVParser parser = new CSVParser(new BufferedReader(new InputStreamReader(Files.newInputStream(filePath))),
-			CSVFormat.DEFAULT.withDelimiter(';').withFirstRecordAsHeader())) {
-
-			for (CSVRecord record : parser) {
-				n++;
-				waitTimeSum += Double.parseDouble(record.get("waitTime"));
-				inVehicleTravelTime.add(Double.parseDouble(record.get("inVehicleTravelTime")));
-				directTravelDistance_m.add(Double.parseDouble(record.get("directTravelDistance_m")));
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
-		double waitTime_s = waitTimeSum / n;
-		System.out.println("Wait time (seconds):" + waitTime_s);
-		System.out.println("Wait time (minutes):" + waitTime_s/60);
-
-		SimpleRegression regression = new SimpleRegression(true);
-
-		for (int i = 0; i < n; i++) {
-			regression.addData(directTravelDistance_m.getDouble(i), inVehicleTravelTime.getDouble(i));
-		}
-
-		// Get the intercept and slope
-		double intercept = regression.getIntercept();
-		double slope = regression.getSlope();
-
-
-		System.out.println("Intercept: " + intercept);
-		System.out.println("Slope: " + slope);
-		System.out.println("R2: " + regression.getRSquare());
-
-		return new EstimatorParameters(intercept, slope, waitTime_s); // Return the parameters as an object
-	}
-
-
-
-	private static void step2_calculateAccessibility(EstimatorParameters estimatorParameters, List<String> relevantPois, ConfigGroup accConfig) {
-
-		// input files
-		String stopsFile = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/kelheim/kelheim-drt-accessibility-JB-master/input/drt-stops-land.xml";
-		String poiFile = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/kelheim/kelheim-drt-accessibility-JB-master/input/pois_complete.csv";
-
-		String eventsFile = ApplicationUtils.matchInput("output_events.xml.gz", Path.of(OUTPUT_DIR)).toString();
-		String networkFile = ApplicationUtils.matchInput("output_network.xml.gz", Path.of(OUTPUT_DIR)).toString();
-		String transportScheduleFile = ApplicationUtils.matchInput("output_transitSchedule.xml.gz", Path.of(OUTPUT_DIR)).toString();
-
-
-		// CONFIG
-		//global
-		final Config config = ConfigUtils.createConfig();
-
-
-		config.controller().setLastIteration(0);
-//		config.controller().setOutputDirectory(utils.getOutputDirectory());
-		config.controller().setOutputDirectory(OUTPUT_DIR);
-		config.controller().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.overwriteExistingFiles);
-
-		config.routing().setRoutingRandomness(0.);
-
-		config.global().setCoordinateSystem("EPSG:25832");
-
-		config.network().setInputFile(networkFile);
-
-
-		// transit
-		config.transit().setTransitScheduleFile(transportScheduleFile);
-
-		// change walk speed to match kelheim scenario
-		config.routing().getTeleportedModeParams().get(TransportMode.walk).setTeleportedModeSpeed(3.8 / 3.6);
-
-		// change scoring default to match kelheim scenario
-		ScoringConfigGroup.ModeParams drtParams = new ScoringConfigGroup.ModeParams(TransportMode.drt);
-		drtParams.setMarginalUtilityOfDistance(-2.5E-4);
-		drtParams.setMarginalUtilityOfTraveling(0.0);
-		config.scoring().addModeParams(drtParams);
-
-		ScoringConfigGroup.ModeParams walkParams = config.scoring().getModes().get(TransportMode.walk);
-		walkParams.setMarginalUtilityOfTraveling(0.0);
-		config.scoring().addModeParams(walkParams);
-
-		// accessibility config
-
-		config.addModule(accConfig);
-
-		// drt config
-		ConfigUtils.addOrGetModule( config, DvrpConfigGroup.class );
-
-		DrtConfigGroup drtConfigGroup = new DrtConfigGroup();
-		drtConfigGroup.operationalScheme = DrtConfigGroup.OperationalScheme.stopbased;
-		drtConfigGroup.transitStopFile = stopsFile;
-
-		drtConfigGroup.addOrGetDrtOptimizationConstraintsParams().addOrGetDefaultDrtOptimizationConstraintsSet().maxWalkDistance = 100000.;
-
-		MultiModeDrtConfigGroup multiModeDrtConfigGroup = new MultiModeDrtConfigGroup();
-		multiModeDrtConfigGroup.addParameterSet(drtConfigGroup);
-		config.addModule(multiModeDrtConfigGroup);
-		config.addModule(drtConfigGroup);
-
-		// SCENARIO
-
-		MutableScenario scenario = (MutableScenario) ScenarioUtils.loadScenario(config);
-
-		for( Link link : scenario.getNetwork().getLinks().values() ){
-			if(!link.getId().toString().startsWith("pt_")) {
-				Set<String> modes = new HashSet<>(link.getAllowedModes());
-				modes.add(TransportMode.walk);
-				modes.add(TransportMode.bike);
-				link.setAllowedModes(modes);
-			}
-
-		}
-
-		// add pois to scenario as facilities
-		readPoiCsv(scenario.getActivityFacilities(), poiFile);
-
-
-		AccessibilityFromEvents.Builder builder = new AccessibilityFromEvents.Builder(scenario, eventsFile, relevantPois);
-
-		// configure DRT Estimator with wait time, and ride time parameters
-		DrtEstimator drtEstimator = new DirectTripDistanceBasedDrtEstimator.Builder()
-			.setWaitingTimeEstimator(new ConstantWaitingTimeEstimator(estimatorParameters.getWaitTime()))
-			.setWaitingTimeDistributionGenerator(new NoDistribution())
-			.setRideDurationEstimator(new ConstantRideDurationEstimator(estimatorParameters.getSlope(), estimatorParameters.getIntercept()))
-			.setRideDurationDistributionGenerator(new NoDistribution())
-			.build();
-
-
-
-		builder.addDrtEstimator(drtEstimator);
-
-
-		builder.build().run();
-
-	}
-
 
 	private static Coordinate transformCoordinate(CoordinateReferenceSystem sourceCRS, CoordinateReferenceSystem targetCRS, Coordinate sourceCoordinate) throws TransformException, FactoryException {
 
@@ -350,12 +351,12 @@ public class RunOfflineAccessibilityKelheim {
 
 			CSVFormat.DEFAULT.withDelimiter(',').withFirstRecordAsHeader())) {
 
-			for (CSVRecord record : parser) {
+			for (CSVRecord csvRecord : parser) {
 
-				String id = record.get("id");
-				double x = Double.parseDouble(record.get("x"));
-				double y = Double.parseDouble(record.get("y"));
-				String type = record.get("type");
+				String id = csvRecord.get("id");
+				double x = Double.parseDouble(csvRecord.get("x"));
+				double y = Double.parseDouble(csvRecord.get("y"));
+				String type = csvRecord.get("type");
 				ActivityFacility fac = af.createActivityFacility(Id.create(id, ActivityFacility.class), new Coord(x, y));
 				ActivityOption ao = af.createActivityOption(type);
 				fac.addActivityOption(ao);
@@ -367,4 +368,33 @@ public class RunOfflineAccessibilityKelheim {
 			throw new RuntimeException(e);
 		}
 	}
+
+	/**
+	 * This class holds the parameters for the DRT estimator, which are generated in step 1.
+	 */
+	public static final class EstimatorParameters {
+		private final double intercept;
+		private final double slope;
+		private final double waitTime;
+
+		public EstimatorParameters(double value1, double value2, double value3) {
+			this.intercept = value1;
+			this.slope = value2;
+			this.waitTime = value3;
+		}
+
+		public double getIntercept() {
+			return intercept;
+		}
+
+		public double getSlope() {
+			return slope;
+		}
+
+		public double getWaitTime() {
+			return waitTime;
+		}
+	}
+
+
 }
